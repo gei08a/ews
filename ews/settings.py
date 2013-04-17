ab090aae02ec8afc2bfbf0a56497e528c26bce56
German Irias <gei08a@acu.edu>
2013-04-17 18:06:19 -0500

    Added local_settings to have different environment in dev and prod.

--- ews/settings.py
+++ ews/settings.py
@@ -1,3 +1,6 @@
+import os
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
 # Django settings for ews project.
 
 DEBUG = True
@@ -12,10 +15,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ews',                      # Or path to database file if using sqlite3.
-        'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': 'django',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'agroalerta',                      # Or path to database file if using sqlite3.
+        'USER': 'agroalerta',                      # Not used with sqlite3.
+        'PASSWORD': 'm0ta6uam0ta6ua',                  # Not used with sqlite3.
+        'HOST': 'db.agroalertahn.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -106,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/gei08a/Projects/ews/templates',
 )
 
 INSTALLED_APPS = (
@@ -150,3 +154,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
